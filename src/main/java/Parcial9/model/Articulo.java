5836c894f3e1579237250cdf6ab8794a298873f3
yorman <sanchezyorman0@gmail.com>
2022-11-23 20:37:57 -0500

    IMPLEMENATCION DE LA PARTE DE ARTICULO

---
+++ src/main/java/Parcial9/model/Articulo.java
@@ -0,0 +1,38 @@
+package Parcial9.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "articulos")
+public class Articulo {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @Column(length = 100)
+    private String nombre;
+
+    @Column(length = 300)
+    private String descripcion;
+
+    private Date fecha_registro;
+
+    @ManyToOne
+    private Categoria categoria;
+
+    @Column(length = 300)
+    private String precio_venta;
+
+    @Column(length = 300)
+    private String precio_compra;
+
+}
